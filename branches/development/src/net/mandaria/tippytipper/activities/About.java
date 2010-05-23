5bebe77e8831897c81e7c0327548f3efb35d24d8
mandlar <mandlar@5fd87178-0ee9-11df-8a19-6d7f5c3cf6d3>
2010-05-23 20:39:54 +0000

    Blank checkin

--- branches/development/src/net/mandaria/tippytipper/activities/About.java
+++ branches/development/src/net/mandaria/tippytipper/activities/About.java
@@ -26,5 +26,6 @@ public class About extends Activity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);  
+        
     }
 }
