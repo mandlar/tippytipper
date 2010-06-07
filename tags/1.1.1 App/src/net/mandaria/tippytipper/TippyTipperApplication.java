f986485103d0518a5ce217ff82f731f2f9874759
mandlar <mandlar@5fd87178-0ee9-11df-8a19-6d7f5c3cf6d3>
2010-06-07 05:51:48 +0000

    Tagging app 1.1.1

---
+++ tags/1.1.1 App/src/net/mandaria/tippytipper/TippyTipperApplication.java
@@ -0,0 +1,10 @@
+package net.mandaria.tippytipper;
+
+import net.mandaria.tippytipper.services.TipCalculatorService;
+import android.app.*;
+
+public class TippyTipperApplication extends Application {
+	
+	public TipCalculatorService service = new TipCalculatorService();
+
+}
