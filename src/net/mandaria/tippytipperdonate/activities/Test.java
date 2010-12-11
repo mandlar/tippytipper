7ed46c3a2c3fd2b526e7afad966eb4bb52f951a6
mandlar <mandlar@users.noreply.github.com>
2010-12-11 16:18:32 +0000

    Merging trunk to donate branch

--- src/net/mandaria/tippytipperdonate/activities/Test.java
+++ src/net/mandaria/tippytipperdonate/activities/Test.java
@@ -1,19 +1,8 @@
 package net.mandaria.tippytipperdonate.activities;
 
 import net.mandaria.tippytipperdonate.R;
-import net.mandaria.tippytipperdonate.R.id;
-import net.mandaria.tippytipperdonate.R.layout;
-import net.mandaria.tippytipperdonate.R.menu;
-import net.mandaria.tippytipperdonate.TippyTipperApplication;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class Test extends Activity  {
 	
